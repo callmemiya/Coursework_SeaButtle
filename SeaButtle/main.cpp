95d6c7c62d8896e41df0f9989a9087a4ae9d0a4b
callmemiya <mari.zabrodina.01@mail.ru>
2020-12-24 21:40:06 +0300

    Initial Commit

---
+++ SeaButtle/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SeaButtle
+//
+//  Created by Мария Забродина on 24/12/2020.
+//  Copyright © 2020 Мария Забродина. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
